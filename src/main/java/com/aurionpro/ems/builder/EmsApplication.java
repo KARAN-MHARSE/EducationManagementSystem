851eaafe97573abe61b3005d513ffb9d9372a2e2
unknown <virti.shah>
2025-07-28 12:33:16 +0530

    Made Minor Change

--- src/main/java/com/aurionpro/ems/builder/EmsApplication.java
+++ src/main/java/com/aurionpro/ems/builder/EmsApplication.java
@@ -1,7 +1,8 @@
-package com.aurionpro.ems;
+package com.aurionpro.ems.builder;
 
 public class EmsApplication{
 	public static void main(String args[]) {
 		System.out.println("Welcome");
+		System.out.println("EMS Project");
 	}
 }
